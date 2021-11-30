e233e2de1ae1e6bf0f61f91ea69a8e0b0996276a
Sachin <sachin97.bs@gmail.com>
2021-11-30 00:29:03 -0500

    Another EDA plot

--- main.R
+++ main.R
@@ -13,13 +13,16 @@ year <- as.numeric(format(d_col, "%Y"))
 
 data2 <- cbind(data2, day, month, year)
 
-############################# EDA ######################################
+############################# EDA #########################################
 temp = data2 %>%
         select(day, total_deaths, new_deaths, month) %>%
         filter(year==2021)
 
 plot_ly(temp, y = ~total_deaths, x = ~month, mode = 'lines', type = 'scatter')
-########################################################################
-
-
+###########################################################################
+new_columns = c("date","total_deaths","new_deaths","total_cases","new_cases", "day","month","year")
+sum_na = colSums(is.na(data2))
+temp = data.frame(sum_na)
+plot_ly(temp, x=new_columns, y=~sum_na, type='bar')
+###########################################################################
 
