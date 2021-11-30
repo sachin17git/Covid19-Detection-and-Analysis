51397d4ae0981b4d6cdb750d52124a9dff5410ff
Sachin <sachin97.bs@gmail.com>
2021-11-29 23:48:09 -0500

    Added a sample EDA snippet

--- main.R
+++ main.R
@@ -1,6 +1,24 @@
 library(data.table)
 library(dplyr)
+library(plotly)
 
 dataset <- fread("owid-covid-data-USA.csv")
 columns <- c("date","total_deaths","new_deaths","total_cases","new_cases")
-data <- select(dataset, colnames<-columns)
+data2 <- select(dataset, colnames<-columns)
+
+d_col <- as.Date(data2$date, format = "%m/%d/%Y")
+day <- as.numeric(format(d_col, "%d"))
+month <- as.numeric(format(d_col, "%m"))
+year <- as.numeric(format(d_col, "%Y"))
+
+data2 <- cbind(data2, day, month, year)
+
+############################# EDA ######################################
+temp = data2 %>%
+        select(day, total_deaths, new_deaths, month) %>%
+        filter(year==2021)
+
+plot_ly(temp, y = ~total_deaths, x = ~month, mode = 'lines', type = 'scatter')
+########################################################################
+
+
