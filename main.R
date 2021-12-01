f6b2019ba9a7409c64f5add4991de49bb990c1c6
Sachin <sachin97.bs@gmail.com>
2021-11-30 21:17:12 -0500

    Dataset split

--- main.R
+++ main.R
@@ -17,14 +17,23 @@ data2 <- cbind(data2, d_col, day, month, year)
 
 ####################### Missing values plot #################################
 
-new_columns = c("date","total_deaths","new_deaths","total_cases","new_cases", "day","month","year")
+new_columns = c("date","total_deaths","new_deaths","total_cases","new_cases","d_col", "day","month","year")
 sum_na = colSums(is.na(data2))
 temp = data.frame(sum_na)
-rownames(temp) = new_columns
+#row.names(temp) = new_columns
 
 temp %>%
-  plot_ly(x=~new_columns,y=~sum_na, type='bar') %>%
+  plot_ly(x=new_columns,y=~sum_na, type='bar') %>%
   layout(title = 'Missing values count')
+
+######################## Fill missing cells ########################################
+
+data2$total_deaths = ifelse(is.na(data2$total_deaths),
+                            0, data2$total_deaths)
+data2$new_deaths = ifelse(is.na(data2$new_deaths),
+                            0, data2$new_deaths)
+data2$new_cases = ifelse(is.na(data2$new_cases),
+                            0, data2$new_cases)
 
 ################ Applying rolling mean for new deaths and cases #######################
 data2 <- data2 %>%
@@ -36,36 +45,40 @@ temp = data2 %>%
         select(new_death_roll12, new_cases_roll12)
 
 row.names(temp) <- data2$d_col
-#plot(temp$new_deaths)
 n_days = 1:dim(temp)[1]
 deathsRoll12=temp$new_death_roll12
 casesRoll12 = temp$new_cases_roll12
+rownames(data2) = data2$d_col
 #plot_ly(temp,x=~n_days,y=~deathsRoll12, mode='lines', type='scatter', text= rownames(temp))
 data2 %>%
-  ggplot(aes(x=n_days, y=deathsRoll12, color='smoker')) +
+  ggplot(aes(x=d_col,y=deathsRoll12, color='smoker')) +
   geom_line() +
-  ggtitle("Rolling mean plot - New Deaths") +
+  ggtitle("                   Rolling mean plot - New Deaths") +
   xlab("Days Jan 2020 - Nov 2021") +
   ylab("New Deaths")
 
 data2 %>%
-  ggplot(aes(x=n_days, y=casesRoll12)) +
-  geom_line(alpha=0.5) +
-  ggtitle("Rolling mean plot - New Cases") +
+  ggplot(aes(x=d_col, y=casesRoll12, color = 'smoker')) +
+  geom_line() +
+  ggtitle("                   Rolling mean plot - New Cases") +
   xlab("Days Jan 2020 - Nov 2021") +
   ylab("New Cases")
 
-######################## Fill missing cells ########################################
-data2$total_deaths = ifelse(is.na(data2$total_deaths),
-                            0, data2$total_deaths)
-data2$new_deaths = ifelse(is.na(data2$new_deaths),
-                            0, data2$new_deaths)
-data2$new_cases = ifelse(is.na(data2$new_cases),
-                            0, data2$new_cases)
-
 ######################### Prepare data for forecasting new deaths #####################
-training_data <- data2 %>%
-                select(new_deaths)
+new_data <- data2 %>%
+  select(new_deaths)
 
-row.names(training_data) = data2$d_col
+row.names(new_data) = data2$d_col
+inds = seq(as.Date("2020-01-22"), as.Date("2021-11-28"), by="day")
+
+set.seed(123)
+new_data_ts = ts(new_data, frequency = 365,
+                 start = c(2020, as.numeric(format(inds[1],"%j"))),
+                 end = c(2021, as.numeric(format(inds[677],"%j"))))
+
+ts.plot(new_data_ts, xlab = "Timeline", ylab = "Deaths", color="red")
+
+training_set = window(new_data_ts, start = c(2020, 22), end = c(2021, 272))
+validation_set = window(new_data_ts, start = c(2021, 273), end = c(2021, 332))
+
 
